91f16cedc14a0cef7237c9beeba1e9c68c5a359a
TomSmulders <91885901+TomSmulders@users.noreply.github.com>
2022-11-17 12:27:13 +0100

    jump1

--- Assets/scripts/playerMovement.cs
+++ Assets/scripts/playerMovement.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public float speed = 10;
-    public float jumpheight = 5;
-    public GameObject bullet;
 
+    public float buttonTime = 0.3f;
+    float jumpTime;
+    bool jumping;
+    float jumpForce = 15;
+
+    public GameObject bullet;
     public float facingDirX = 1;
 
     // Start is called before the first frame update
@@ -24,7 +28,6 @@ public class playerMovement : MonoBehaviour
         
 
         transform.Translate(transform.right * dirX * speed * Time.deltaTime);
-        transform.Translate(transform.up * dirY * jumpheight * Time.deltaTime);
 
         if (dirX == -1 || dirX ==1)
         {
@@ -36,5 +39,23 @@ public class playerMovement : MonoBehaviour
             GameObject spawnedBullet = Instantiate(bullet, transform.position, Quaternion.identity);
             spawnedBullet.GetComponent<bullet>().dirX = facingDirX;
         }
+
+        //jump https://www.youtube.com/watch?v=c9kxUvCKhwQ
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumping = true;
+            jumpTime = 0;
+        }
+        if (jumping)
+        {
+            jumpTime += Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.Space) | jumpTime > buttonTime)
+        {
+            jumping = false;
+        }
+        
+        Rigidbody2D.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 }
